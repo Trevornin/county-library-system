7547391cc7a61d260707e81d7c33f62d3ea5e85b
Trevornin <87859826+Trevornin@users.noreply.github.com>
2021-07-23 15:33:19 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*-----------------------------
 * Version: C99                *
 * Project: Library management *
-* Author:  Titus Njiru        *
+* Author:  Omoke Edgah        *
 * Date:    June,2021          *
 * License: MIT                *
 ******************************/
@@ -14,7 +14,7 @@ int main()
 {
     int action;
     printf("\tCounty Library Management System!\n");
-    printf("Welcome Titus\n");
+    printf("Welcome Edgah\n");
     // Navigation menu
     do {
         printf("1. Add user\n");
